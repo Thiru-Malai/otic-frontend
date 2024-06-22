729cb1973e7da9321e116f02e25b9e8c15eda483
Thirumalai <malaithiru370@gmail.com>
2024-06-22 23:21:13 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'otic',
+  webDir: 'www'
+};
+
+export default config;
